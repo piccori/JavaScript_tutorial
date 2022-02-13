25bd1b6b778479c71617fc04664517eee7682270
HigaTaisei <taisei.higa.1999@icloud.com>
2022-02-13 14:01:26 +0900

    配列 #38

--- Foundation/Arr.js
+++ Foundation/Arr.js
@@ -1,4 +1,4 @@
-// 配列：値に順序をつけて書くのできるオブジェクト
+// 配列：値に順序をつけて書くことのできるオブジェクト
 // 配列の作成とアクセス
 const emptyArray = [];
 const numbers = [1, 2, 3];
@@ -105,3 +105,124 @@ const NewArray = newArray.concat("新しい要素");
 console.log(NewArray);
 
 // 配列の展開
+const arry1 = ["A", "B", "C"];
+const newArray1 = ["X", "y", "Z", ...arry1];
+const newArrayConcat = ["X", "Y", "Z"].concat(arry1);
+console.log(newArray1);
+console.log(newArrayConcat);
+
+// 配列をフラット化
+const arry3 = [[["A"], "B"], "C"];
+console.log(arry3.flat());
+console.log(arry3.flat(1));
+console.log(arry3.flat(2));
+console.log(arry3.flat(Infinity));
+
+//Array.prototype.splice
+console.log(newArray);
+newArray.splice(1, 1);
+console.log(newArray);
+newArray.splice(0, newArray.length);
+console.log(newArray);
+
+// lengthプロパティへの代入
+const num_arr = [1, 2, 3];
+num_arr.length = 0;
+console.log(num_arr);
+
+//　破壊的なオブジェクト：配列オブジェクトそのものを変更し、変更した配列又は変更箇所を返すメソッド
+// 非破壊的メソッド：配列オブジェクトのコピーを作成してから変更し、そのコピーした配列を返すメソッド
+
+// 破壊的メソッド push
+const myArray = ["A", "B", "C"];
+const result = myArray.push("D");
+console.log(result);
+console.log(myArray);
+
+// 非破壊的メソッド
+const myArray_str = ["A", "B", "C"];
+const newArray_str = myArray_str.concat("D");
+console.log(newArray_str);
+console.log(myArray_str);
+console.log(myArray_str === newArray_str);
+
+/*
+破壊的メソッド
+Array.prototype.pop	配列の末尾の値
+Array.prototype.push	変更後の配列のlength
+Array.prototype.splice	取り除かれた要素を含む配列
+Array.prototype.reverse	反転した配列
+Array.prototype.shift	配列の先頭の値
+Array.prototype.sort	ソートした配列
+Array.prototype.unshift	変更後の配列のlength
+Array.prototype.copyWithin[ES2015]	変更後の配列
+Array.prototype.fill[ES2015]	変更後の配列
+*/
+
+// forEach
+const num_arry = [1, 2, 3];
+num_arry.forEach((currentValue, index, num_arry) => {
+  console.log(currentValue, index, num_arry);
+});
+
+// map
+const num_newArray = num_arry.map((currentValue, index, num_arry) => {
+  return currentValue * 10;
+});
+console.log(num_newArray);
+console.log(num_arry === num_newArray);
+
+// filter
+const newArrayFilter = num_arry.filter((currentValue, index, num_arry) => {
+  return currentValue % 2 === 1;
+});
+console.log(newArrayFilter);
+console.log(num_arry === newArrayFilter);
+
+// reduce
+const totalValue = num_arry.reduce(
+  (accumulator, currentValue, index, num_arry) => {
+    return accumulator + currentValue;
+  },
+  0
+);
+console.log(totalValue);
+
+// Array_likeオブジェクト
+function myFunc() {
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments[2]);
+  console.log(typeof arguments.forEach);
+}
+myFunc("a", "b", "c");
+
+// Array_likeオブジェクトの判定
+function myFunc1() {
+  const argumentsArray = Array.from(arguments);
+  console.log(Array.isArray(argumentsArray));
+  argumentsArray.forEach((arg) => {
+    console.log(arg);
+  });
+}
+myFunc1("a", "b", "c");
+
+// メソッドチェーンと高階関数
+const arrayConcat = ["a"].concat("b").concat("c");
+console.log(arrayConcat);
+
+const ECMAScriptVersions = [
+  { name: "ECMAScript 1", year: 1997 },
+  { name: "ECMAScript 2", year: 1998 },
+  { name: "ECMAScript 3", year: 1999 },
+  { name: "ECMAScript 5", year: 2009 },
+  { name: "ECMAScript 5.1", year: 2011 },
+  { name: "ECMAScript 2015", year: 2015 },
+  { name: "ECMAScript 2016", year: 2016 },
+  { name: "ECMAScript 2017", year: 2017 },
+];
+
+const versionNames = ECMAScriptVersions.filter(
+  (ECMAScript) => ECMAScript.year <= 2000
+).map((ECMAScript) => ECMAScript.name);
+console.log(versionNames);
