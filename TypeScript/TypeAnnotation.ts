658ccba22e6aefe6ed066a8f147818cc395ebdd2
HigaTaisei <taisei.higa.1999@icloud.com>
2022-02-15 21:13:08 +0900

    TypeScript React

---
+++ TypeScript/TypeAnnotation.ts
@@ -0,0 +1,29 @@
+const num: number = 123;
+
+const isOk: boolean = true;
+
+const count: number = 123;
+
+const hello: string = "Hello";
+const world: string = "World!";
+console.log(hello + world);
+
+const x: null = null;
+
+const y: undefined = undefined;
+
+const s: Symbol = Symbol("foo");
+
+const big: bigint = 100n;
+
+// ラッパーオブジェクト
+const bool: Boolean = false;
+const num_zero: Number = 0;
+const str1: String = "";
+const sym: Symbol = Symbol();
+const big1: BigInt = 10n;
+
+let value: any;
+value = 1;
+value = "string";
+value = { name: "オブジェクト" };
